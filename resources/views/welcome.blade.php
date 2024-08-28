b47c4e0a476b3717a212de3418f5569a838c475f
FaizNala <faiznala2@gmail.com>
2024-08-28 21:55:03 +0700

    tugas ktp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,71 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getlocale())}}">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PWL 2024</title>
+    <title>KTP</title>
 </head>
 <body>
-    <h2>Pemrograman Web Lanjut 2024</h2>
-    <h3>Semangat Belajar Laravel 10</h3>
+
+    <table border="1" cellpadding="10" cellspacing="0"  align="center">
+        <tr>
+            <td colspan="2" align="center" style="border:none">
+                <strong>PROVINSI JAWA TIMUR</strong><br>
+                <strong>KABUPATEN NGANJUK</strong>
+            </td>
+        </tr>
+        <tr>
+            <td width="100" style="border:none" width><strong>NIK</strong></td>
+            <td width="300" style="border:none"><strong>: 3518141002030001</strong></td>
+        </tr>
+        <tr>
+            <td width="100" style="border:none"><strong>Nama</strong></td>
+            <td width="300" style="border:none"><strong>: FAIZ NALA SAMUDERA</strong></td>
+        </tr>
+        <tr>
+            <td width="100" style="border:none"><strong>Tempat/Tgl Lahir</strong></td>
+            <td width="300" style="border:none"><strong>: NGANJUK, 10-02-2003</strong></td>
+        </tr>
+        <tr>
+            <td width="100" style="border:none"><strong>Jenis Kelamin</strong></td>
+            <td width="300" style="border:none"><strong>: LAKI-LAKI</strong></td>
+        </tr>
+        <tr>
+            <td width="200" style="border:none"><strong>Alamat</strong></td>
+            <td width="400" style="border:none"><strong>: DSN. SELOLOR</strong></td>
+        </tr>
+        <tr>
+            <td width="200" style="border:none"><strong>RT/RW</strong></td>
+            <td width="400" style="border:none"><strong>: 005/002</strong></td>
+        </tr>
+        <tr>
+            <td width="200" style="border:none"><strong>Kel/Desa</strong></td>
+            <td width="400" style="border:none"><strong>: SELOREJO</strong></td>
+        </tr>
+        <tr>
+            <td width="200" style="border:none"><strong>Kecamatan</strong></td>
+            <td width="400" style="border:none"><strong>: BAGOR</strong></td>
+        </tr>
+        <tr>
+            <td width="200" style="border:none"><strong>Agama</strong></td>
+            <td width="400" style="border:none"><strong>: ISLAM</strong></td>
+        </tr>
+        <tr>
+            <td width="200" style="border:none"><strong>Status Perkawinan</strong></td>
+            <td width="400" style="border:none"><strong>: BELUM KAWIN</strong></td>
+        </tr>
+        <tr>
+            <td width="200" style="border:none"><strong>Pekerjaan</strong></td>
+            <td width="400" style="border:none"><strong>: MAHASISWA</strong></td>
+        </tr>
+        <tr>
+            <td width="200" style="border:none"><strong>Kewarganegaraan</strong></td>
+            <td width="400" style="border:none"><strong>: WNI</strong></td>
+        </tr>
+        <tr>
+            <td width="200" style="border:none"><strong>Berlaku Hingga</strong></td>
+            <td width="400" style="border:none"><strong>: SEUMUR HIDUP</strong></td>
+        </tr>
+    </table>
 </body>
 </html>
