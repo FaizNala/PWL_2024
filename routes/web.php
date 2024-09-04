4f42385aaaee8c88ba3826053618fd3e99298552
FaizNala <faiznala2@gmail.com>
2024-09-04 14:07:50 +0700

    Pertemuan 2 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return 'Hello, World!';
+});
+
+Route::get('/world', function() {
+    return 'World';
+});
+
+Route::get('/datang', function() {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function() {
+    return 'NIM : 2241760043 <br> Nama : Faiz Nala Samudera';
+});
+
+Route::get('/user/{name}', function($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+}); 
+
+Route::get('/article/{id}', function($id){
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name; 
+}); 
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name; 
+}); 
